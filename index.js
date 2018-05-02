6109cbb1a21e8914491a2ab89e90a963c9b966fa
Xavier Urzúa <xavier.urzua.acuna@gmail.com>
2018-05-02 17:25:36 -0300

    Repo initialization

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+const PORT = 3000
+
+app.use(express.static('public'))
+app.listen(PORT, () => console.log(`SpeechRecognition demo app is listening on port ${PORT}!`))
