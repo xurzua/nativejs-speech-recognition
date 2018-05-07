51d606cf2c6ca5e313c96700e9b18b05a3c2b00a
Xavier Urzúa <xavier.urzua.acuna@gmail.com>
2018-05-07 16:31:25 -0300

    modified browser console output messages for

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
-const PORT = 3000;
+const PORT = 3000
 
-app.use(express.static("public"));
+app.use(express.static('public'))
 app.listen(PORT, () =>
-  console.log(`SpeechRecognition demo app is listening on port ${PORT}!`)
-);
+    console.log(`SpeechRecognition demo app is listening on port ${PORT}!`)
+)
